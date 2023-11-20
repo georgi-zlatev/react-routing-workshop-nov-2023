a8189c9e14e200407dc971ec429ff8a0b8d7acc9
Georgi Zlatev <georgi.n.zlatev@gmail.com>
2023-11-20 22:37:01 +0200

    Add react router lib, add All games list

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
+import GameList from "./components/GameList/GameList";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
 
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/games" element={<GameList />} />
       </Routes>
     </div>
   );
